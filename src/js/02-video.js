6831cf730e41970bb2def7ce074191e57b956609
Eugene Hlushak <hlushak.e@gmail.com>
2022-12-16 08:46:00 +0200

    add player
    install vimeo player
    add script for player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', videoCurrentTime);
+
+function videoCurrentTime(e) {
+  localStorage.setItem('videoCurrentTime', JSON.stringify(e));
+  const time = localStorage.getItem('videoCurrentTime');
+}

fe88121439bea886bc8c9528d28a8da97ff2fa5a
Eugene Hlushak <hlushak.e@gmail.com>
2022-12-17 00:57:11 +0200

    add script for feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputMail = document.querySelector('input');
+const inputTextarea = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+autoCompleteInput();
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  try {
+    const userData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(userData);
+  } catch (error) {}
+  e.preventDefault();
+  e.currentTarget.reset();
+}
+
+function autoCompleteInput() {
+  try {
+    const a = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (a) {
+      inputMail.value = a.email;
+      inputTextarea.value = a.message;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
